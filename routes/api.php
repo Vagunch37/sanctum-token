708538c18a7b2ad9f51111a65f0078c0d7df44ff
Vagunch37 <777vahagn777@gmail.com>
2021-02-04 14:17:33 +0400

    Third

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ use App\Http\Controllers\UserController;
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
 
-
+//
 Route::middleware('auth:api')->group(function () {
     Route::get('user', [UserController::class, 'user']);
     Route::get('userTokens', [UserController::class, 'userTokens']);
